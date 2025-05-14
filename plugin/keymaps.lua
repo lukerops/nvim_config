58c5015c3a91d30f74029dfb67a33d53ff5e98f7
Lucas Campos Vieira <contato@lukerops.com>
2025-05-14 17:15:36 -0300

    adiciona keymaps de mover linhas

---
+++ plugin/keymaps.lua
@@ -0,0 +1,6 @@
+vim.keymap.set("n", "<M-Up>", ":m .-2<CR>==")
+vim.keymap.set("n", "<M-Down>", ":m .+1<CR>==")
+vim.keymap.set("i", "<M-Up>", "<Esc>:m .-2<CR>==gi")
+vim.keymap.set("i", "<M-Down>", "<Esc>:m .+1<CR>==gi")
+vim.keymap.set("v", "<M-Up>", ":m '<-2<CR>gv=gv")
+vim.keymap.set("v", "<M-Down>", ":m '>+1<CR>gv=gv")
