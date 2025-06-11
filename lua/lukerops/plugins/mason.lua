ba8a4e754aee90ed8f4afcbb86a8d6261aa6d2c7
Lucas Campos Vieira <contato@lukerops.com>
2025-06-11 14:44:20 -0300

    adiciona repositório local e os pacotes hurl e ripgrep

--- lua/lukerops/plugins/mason.lua
+++ lua/lukerops/plugins/mason.lua
@@ -4,11 +4,21 @@ return {
   "williamboman/mason.nvim",
   -- event = "VeryLazy",
   cmd = { "Mason" },
-  opts = {
-    ui = { border = config.ui.border },
-    registries = {
-        "github:mason-org/mason-registry",
-	-- "file:" .. vim.fn.stdpath("config") .. "/mason-registry"
-    },
-  },
+  opts = function()
+    local registries = {"github:mason-org/mason-registry"}
+
+    if vim.fn.executable("yq") ~= 1 then
+      vim.notify(
+        "Instale o yq para usar o Mason com suporte a repositorio local.",
+        vim.log.levels.WARN
+      )
+    else
+      table.insert(registries, "file:" .. vim.fn.stdpath("config") .. "/mason-registry")
+    end
+
+    return {
+      ui = { border = config.ui.border },
+      registries = registries,
+    }
+  end,
 }
