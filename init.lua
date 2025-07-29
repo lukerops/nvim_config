d49fddc877f9cd26a7d43885ea487ab192494b86
Lucas Campos Vieira <contato@lukerops.com>
2025-07-29 17:58:42 -0300

    remove o lazyload dos plugins

--- init.lua
+++ init.lua
@@ -1,106 +1,5 @@
 local border = 'rounded'
 
----------------
---- plugins ---
----------------
-
-local plugins = {
-  {
-    'folke/tokyonight.nvim',
-    lazy = true,
-    opts = {
-      style = 'night',
-    },
-  },
-  {
-    'stevearc/oil.nvim',
-    dependencies = { 'nvim-tree/nvim-web-devicons' },
-    keys = {
-      { '<leader>fe', '<cmd>Oil<cr>', desc = 'File Explorer' },
-    },
-    opts = { float = { border = border } },
-  },
-  {
-    'nvim-telescope/telescope.nvim',
-    tag = '0.1.8',
-    dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
-    cmd = { 'Telescope' },
-    keys = {
-      { '<leader>/',  '<cmd>Telescope live_grep<cr>',       desc = 'Find in Files (Grep)' },
-      { '<leader>ff', '<cmd>Telescope find_files<cr>',      desc = 'Find Files' },
-      { '<leader>fb', '<cmd>Telescope buffers<cr>',         desc = 'Buffers' },
-      { '<leader>ch', '<cmd>Telescope command_history<cr>', desc = 'Command History' },
-      { '<leader>km', '<cmd>Telescope keymaps<cr>',         desc = 'Key Maps' },
-    },
-    config = function()
-      local actions = require('telescope.actions')
-
-      require('telescope').setup({
-        defaults = {
-          vimgrep_arguments = {
-            'rg', '--color=never', '--no-heading', '--with-filename',
-            '--line-number', '--column', '--smart-case', '--hidden',
-            '--glob', '!**/.git/*',
-          },
-          mappings = {
-            i = {
-              ['<C-Up>'] = actions.preview_scrolling_up,
-              ['<C-Down>'] = actions.preview_scrolling_down,
-            },
-            n = {
-              ['<C-Up>'] = actions.preview_scrolling_up,
-              ['<C-Down>'] = actions.preview_scrolling_down,
-            },
-          },
-        },
-        pickers = {
-          find_files = {
-            -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
-            find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
-          },
-          buffers = {
-            ignore_current_buffer = true,
-            sort_mru = true,
-            sort_lastused = true,
-            mappings = {
-              i = {
-                ['<C-d>'] = actions.delete_buffer + actions.move_to_top,
-              },
-              n = {
-                ['<C-d>'] = actions.delete_buffer + actions.move_to_top,
-              },
-            },
-          },
-        },
-      })
-    end,
-  },
-  {
-    'nvim-treesitter/nvim-treesitter',
-    main = 'nvim-treesitter.configs',
-    build = ':TSUpdate',
-    event = 'BufReadPost',
-    dependencies = { 'nvim-treesitter/nvim-treesitter-textobjects' },
-    opts = {
-      ensure_installed = {
-        'go', 'javascript', 'typescript', 'python', 'lua', 'bash',
-        'graphql', 'json', 'yaml', 'toml',
-        'dockerfile', 'hcl', 'terraform',
-      },
-      highlight = { enable = true },
-      indent = { enable = true },
-    },
-  },
-  {
-    'neovim/nvim-lspconfig',
-    cmd = { 'LspInfo', 'LspInstall', 'LspStart' },
-    event = { 'BufReadPre', 'BufNewFile' },
-    config = function()
-      vim.lsp.enable({ 'ts_ls' })
-    end,
-  }
-}
-
 -----------------
 --- lazy.nvim ---
 -----------------
@@ -132,13 +31,117 @@ vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
 require('lazy').setup({
-  spec = { plugins },
+  spec = {
+    {
+      'folke/tokyonight.nvim',
+      opts = { style = 'night' },
+    },
+    {
+      'stevearc/oil.nvim',
+      dependencies = { 'nvim-tree/nvim-web-devicons' },
+    },
+    {
+      'nvim-telescope/telescope.nvim',
+      tag = '0.1.8',
+      dependencies = { 'nvim-lua/plenary.nvim', 'nvim-tree/nvim-web-devicons' },
+    },
+    'nvim-treesitter/nvim-treesitter',
+    'neovim/nvim-lspconfig',
+    'zbirenbaum/copilot.lua',
+    'MeanderingProgrammer/render-markdown.nvim',
+    'olimorris/codecompanion.nvim',
+  },
   -- Configure any other settings here. See the documentation for more details.
   -- colorscheme that will be used when installing plugins.
   -- install = { colorscheme = { 'habamax' } },
   -- automatically check for plugin updates
   checker = { enabled = true },
   ui = { border = border },
+})
+
+---------------
+--- plugins ---
+---------------
+
+vim.lsp.enable({ 'ts_ls' })
+
+require('oil').setup({ float = { border = border } })
+require('render-markdown').setup({
+  completions = { lsp = { enabled = true } },
+  file_types = { 'markdown', 'codecompanion' },
+})
+
+local actions = require('telescope.actions')
+
+require('telescope').setup({
+  defaults = {
+    vimgrep_arguments = {
+      'rg', '--color=never', '--no-heading', '--with-filename',
+      '--line-number', '--column', '--smart-case', '--hidden',
+      '--glob', '!**/.git/*',
+    },
+    mappings = {
+      i = {
+        ['<C-Up>'] = actions.preview_scrolling_up,
+        ['<C-Down>'] = actions.preview_scrolling_down,
+      },
+      n = {
+        ['<C-Up>'] = actions.preview_scrolling_up,
+        ['<C-Down>'] = actions.preview_scrolling_down,
+      },
+    },
+  },
+  pickers = {
+    find_files = {
+      -- `hidden = true` will still show the inside of `.git/` as it's not `.gitignore`d.
+      find_command = { 'rg', '--files', '--hidden', '--glob', '!**/.git/*' },
+    },
+    buffers = {
+      ignore_current_buffer = true,
+      sort_mru = true,
+      sort_lastused = true,
+      mappings = {
+        i = { ['<C-d>'] = actions.delete_buffer + actions.move_to_top },
+        n = { ['<C-d>'] = actions.delete_buffer + actions.move_to_top },
+      },
+    },
+  },
+})
+
+require('nvim-treesitter.configs').setup({
+  ensure_installed = {
+    'go', 'javascript', 'typescript', 'python', 'lua', 'bash',
+    'graphql', 'json', 'yaml', 'toml',
+    'dockerfile', 'hcl', 'terraform',
+  },
+  highlight = { enable = true },
+  indent = { enable = true },
+})
+
+require('copilot').setup({
+  suggestion = { enabled = true, auto_trigger = true },
+  panel = { enabled = false },
+})
+
+require('codecompanion').setup({
+  display = {
+    action_palette = { provider = 'telescope' },
+    chat = {
+      start_in_insert_mode = true,
+      window = {
+        layout = 'vertical',
+        position = 'right',
+        border = border,
+        width = 0.3,
+        opts = {
+          -- https://neovim.io/doc/user/options.html#'number'
+          number = false,
+          relativenumber = false,
+        }
+      },
+    },
+  },
+  strategies = { chat = { adapter = 'copilot' } },
 })
 
 --------------------
@@ -188,6 +191,15 @@ vim.keymap.set('n', '<c-w>d', function()
   vim.diagnostic.open_float({ border = border })
 end, { desc = 'Show Diagnostics' })
 
+vim.keymap.set('n', '<leader>fe', '<cmd>Oil<cr>', { desc = 'File Explorer' })
+vim.keymap.set('n', '<leader>/', '<cmd>Telescope live_grep<cr>', { desc = 'Find in Files (Grep)' })
+vim.keymap.set('n', '<leader>ff', '<cmd>Telescope find_files<cr>', { desc = 'Find Files' })
+vim.keymap.set('n', '<leader>fb', '<cmd>Telescope buffers<cr>', { desc = 'Buffers' })
+vim.keymap.set('n', '<leader>ch', '<cmd>Telescope command_history<cr>', { desc = 'Command History' })
+vim.keymap.set('n', '<leader>/', '<cmd>Telescope keymaps<cr>', { desc = 'Key Maps' })
+
+vim.keymap.set('n', '<leader>ai', '<cmd>CodeCompanionChat Toggle<cr>', { desc = 'Toggle Code Companion Chat' })
+
 --------------------
 --- autocommands ---
 --------------------
@@ -208,5 +220,26 @@ vim.api.nvim_create_autocmd('LspAttach', {
     bufmap('n', 'K', function() vim.lsp.buf.hover({ border = border }) end)
     bufmap('i', '<c-space>', function() vim.lsp.completion.get() end)
     bufmap({ 'i', 's' }, '<c-s>', function() vim.lsp.buf.signature_help({ border = border }) end)
+    bufmap('n', 'gd', function() vim.lsp.buf.definition() end)
+  end,
+})
+
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'codecompanion' },
+  callback = function(event)
+    vim.keymap.set('n', '<leader>bff', function()
+      local chat = require('codecompanion').buf_get_chat(event.buf)
+      local completion = require('codecompanion.providers.completion')
+
+      for _, command in ipairs(require('codecompanion.providers.completion').slash_commands()) do
+        if command.label == '/buffer' then
+          completion.slash_commands_execute(command, chat)
+          return
+        end
+      end
+    end, {
+      buffer = event.buf,
+      desc = 'Code Companion: Find Files',
+    })
   end,
 })
