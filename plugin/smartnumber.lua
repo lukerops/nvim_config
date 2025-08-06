f1430d51663acdc6161b1423b606ff34e1a353b6
Lucas Campos Vieira <contato@lukerops.com>
2025-08-05 22:22:58 -0300

    adiciona plugin smartnumber
    adiciona plugin para habilitar e desabilitar o relativenumber

---
+++ plugin/smartnumber.lua
@@ -0,0 +1,13 @@
+local group_id = vim.api.nvim_create_augroup('smartnumber', { clear = true})
+
+vim.api.nvim_create_autocmd('InsertEnter', {
+  group = group_id,
+  pattern = { '*' },
+  callback = function() vim.opt.relativenumber = false end,
+})
+
+vim.api.nvim_create_autocmd('InsertLeave', {
+  group = group_id,
+  pattern = { '*' },
+  callback = function() vim.opt.relativenumber = true end,
+})
