b7279d0da6db89269eca9225729e6cd267e261b4
Lucas Campos Vieira <contato@lukerops.com>
2025-08-13 18:29:32 -0300

    desabilita o smartnumber

--- plugin/smartnumber.lua
+++ plugin/smartnumber.lua
@@ -1,13 +1,16 @@
+-- TODO: ess código está bugando a exibição de números relativos
+-- em filetypes que antes não tinham números de linha
+-- Ex: terminal e codecompanion
 local group_id = vim.api.nvim_create_augroup('smartnumber', { clear = true})
 
-vim.api.nvim_create_autocmd('InsertEnter', {
-  group = group_id,
-  pattern = { '*' },
-  callback = function() vim.opt.relativenumber = false end,
-})
-
-vim.api.nvim_create_autocmd('InsertLeave', {
-  group = group_id,
-  pattern = { '*' },
-  callback = function() vim.opt.relativenumber = true end,
-})
+-- vim.api.nvim_create_autocmd('InsertEnter', {
+--   group = group_id,
+--   pattern = { '*' },
+--   callback = function() vim.opt.relativenumber = false end,
+-- })
+--
+-- vim.api.nvim_create_autocmd('InsertLeave', {
+--   group = group_id,
+--   pattern = { '*' },
+--   callback = function() vim.opt.relativenumber = true end,
+-- })
