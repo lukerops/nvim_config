0efa5f88d59856b90a33214c598c9a36ecf7c4bd
Lucas Campos Vieira <contato@lukerops.com>
2025-08-18 10:25:46 -0300

    adiciona mcphub

--- lua/plugin/codecompanion.lua
+++ lua/plugin/codecompanion.lua
@@ -19,9 +19,19 @@ return {
             number = false,
             relativenumber = false,
           }
-        },
-      },
+        }
+      }
     },
     strategies = { chat = { adapter = 'copilot' } },
+    extensions = {
+      mcphub = {
+        callback = "mcphub.extensions.codecompanion",
+        opts = {
+          make_vars = true,
+          make_slash_commands = true,
+          show_result_in_chat = true,
+        }
+      }
+    }
   }
 }
---
+++ lua/plugin/mcphub.lua
@@ -0,0 +1,7 @@
+return {
+  'ravitemer/mcphub.nvim',
+  tag = 'v6.2.0',
+  build = 'npm install -g mcp-hub@latest',
+  cmd = 'MCPHub',
+  opts = {},
+}
