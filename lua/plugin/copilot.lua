d18a374088e33365ac5eac39fc3b1502a807438f
Lucas Campos Vieira <contato@lukerops.com>
2025-09-18 14:44:53 -0300

    limita o autocomplete do copilot ao typescript

--- lua/plugin/copilot.lua
+++ lua/plugin/copilot.lua
@@ -1,5 +1,7 @@
 return {
   'zbirenbaum/copilot.lua',
+  -- event = { 'BufReadPre', 'BufNewFile' },
+  ft = { 'typescript' },
   opts = {
     suggestion = { enabled = true, auto_trigger = true },
     panel = { enabled = false },
