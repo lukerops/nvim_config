4c4f955d789d737bb5289ddaeb8c20d7466a3106
Lucas Campos Vieira <contato@lukerops.com>
2025-09-18 14:45:49 -0300

    ajusta dependência do telescope

--- lua/plugin/telescope.lua
+++ lua/plugin/telescope.lua
@@ -1,6 +1,7 @@
 return {
   'nvim-telescope/telescope.nvim',
   tag = '0.1.8',
+  dependencies = { 'nvim-lua/plenary.nvim' },
   keys = {
     { '<leader>/', '<cmd>Telescope live_grep<cr>', desc = 'Find in Files (Grep)' },
     { '<leader>ff', '<cmd>Telescope find_files<cr>', desc = 'Find Files' },
